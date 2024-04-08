a0025b37cfad74b151a5156c217ff5fd6b2ebe62
YashBhola <95426334+YashPro8158@users.noreply.github.com>
2024-04-08 20:29:03 +0530

    Gastronomy Live

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let hamburger = document.getElementById("hamburger");
+let navbar = document.getElementById("navbar");
+hamburger.addEventListener("click",function () {
+    navbar.classList.toggle("navactive")
+})
